0523f7ef5490a73471dc3469e60bf04b216f0961
Richard Iannone <riannone@me.com>
2022-08-23 12:31:46 -0400

    Rename and update code

--- ARD/make_pop.R
+++ ARD/make_pop.R
@@ -1,36 +1,43 @@
 library(safetyData)
 library(tidyverse)
 
+adam_adsl <-
+  adam_adsl %>%
+  mutate(TRT01A = "Total") %>%
+  bind_rows(adam_adsl)
 
 # Big N
-big_n <- adam_adsl %>%
+big_n <-
+  adam_adsl %>%
   group_by(TRT01A) %>%
   summarise(N = n())
 
-long_big_n <- big_n %>%
+long_big_n <-
+  big_n %>%
   pivot_longer(-TRT01A, names_to = "param")
 
 # Population n's
 map(adam_adsl, function(x){
   attr(x, "label")
 })
-adam_adsl %>%
+
+pop_ard <-
+  adam_adsl %>%
   group_by(TRT01A) %>%
   summarise(across(c(SAFFL, ITTFL, EFFFL, starts_with("COMP")),
                    ~sum(. == "Y"))) %>%
   pivot_longer(-TRT01A) %>%
   left_join(big_n, by = "TRT01A") %>%
   mutate(pct = value/N*100) %>%
-  select(-N, n= value) %>%
+  select(-N, n = value) %>%
   pivot_longer(c("n", "pct"), names_to = "param") %>%
-  case_when(
+  mutate(name = case_when(
     name == "SAFFL" ~ "Safety Population",
     name == "ITTFL" ~ "Intent-To-Treat Population",
     name == "EFFFL" ~ "Efficacy Population",
     name == "COMP8FL" ~ "Completers of Week 8 Population",
     name == "COMP16FL" ~ "Completers of Week 16 Population",
-    name == "COMP24FL" ~ "Completers of Week 24 Population",
-
-  )
-
-
+    name == "COMP24FL" ~ "Completers of Week 24 Population"
+    )
+  ) %>%
+  bind_rows(long_big_n)
